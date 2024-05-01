00392be673b7c3768b57783d12956349f9046c95
yvesmarin <johnyvesmarin@gmail.com>
2024-05-02 01:57:37 +0800

    Update portfolio details and added needed data

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,99 +1,103 @@
-"use client";
-import React, { useTransition, useState } from "react";
-import Image from "next/image";
-import TabButton from "./TabButton";
+'use client'
+import React, { useTransition, useState } from 'react'
+import Image from 'next/image'
+import TabButton from './TabButton'
 
 const TAB_DATA = [
   {
-    title: "Skills",
-    id: "skills",
+    title: 'Skills',
+    id: 'skills',
     content: (
-      <ul className="list-disc pl-2">
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>PostgreSQL</li>
-        <li>Sequelize</li>
+      <ul className='list-disc pl-2'>
+        <li>HTML</li>
+        <li>CSS</li>
         <li>JavaScript</li>
         <li>React</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>MongoDB</li>
       </ul>
-    ),
+    )
   },
   {
-    title: "Education",
-    id: "education",
+    title: 'Education',
+    id: 'education',
     content: (
-      <ul className="list-disc pl-2">
-        <li>Fullstack Academy of Code</li>
-        <li>University of California, Santa Cruz</li>
+      <ul className='list-disc pl-2'>
+        <li>BS Information Systems - La Verdad Christian College</li>
       </ul>
-    ),
+    )
   },
   {
-    title: "Certifications",
-    id: "certifications",
+    title: 'Certifications',
+    id: 'certifications',
     content: (
-      <ul className="list-disc pl-2">
-        <li>AWS Cloud Practitioner</li>
-        <li>Google Professional Cloud Developer</li>
+      <ul className='list-disc pl-2'>
+        <li>FreeCodeCamp - Responsive Web Design</li>
+        <li>SoloLearn - HTML, Javascript, React and Redux</li>
       </ul>
-    ),
-  },
-];
+    )
+  }
+]
 
 const AboutSection = () => {
-  const [tab, setTab] = useState("skills");
-  const [isPending, startTransition] = useTransition();
+  const [tab, setTab] = useState('skills')
+  const [isPending, startTransition] = useTransition()
 
-  const handleTabChange = (id) => {
+  const handleTabChange = id => {
     startTransition(() => {
-      setTab(id);
-    });
-  };
+      setTab(id)
+    })
+  }
 
   return (
-    <section className="text-white" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+    <section className='text-white' id='about'>
+      <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
+        <Image
+          src='/images/about-image.png'
+          alt='About Image'
+          width={500}
+          height={500}
+        />
+        <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
+          <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
+          <p className='text-base lg:text-lg'>
+            I am an aspiring full-stack web developer with a passion for
+            creating interactive and responsive web applications. I have
+            experience working with JavaScript, React, Redux, Node.js, Express,
+            MongoDB, HTML, CSS, and Git. I am a quick learner, and I am always
+            looking to expand my knowledge and skill set. I am a team
+            player, and I am excited to work with others to
+            create amazing applications.
           </p>
-          <div className="flex flex-row justify-start mt-8">
+          <div className='flex flex-row justify-start mt-8'>
             <TabButton
-              selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}
+              selectTab={() => handleTabChange('skills')}
+              active={tab === 'skills'}
             >
-              {" "}
-              Skills{" "}
+              {' '}
+              Skills{' '}
             </TabButton>
             <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
+              selectTab={() => handleTabChange('education')}
+              active={tab === 'education'}
             >
-              {" "}
-              Education{" "}
+              {' '}
+              Education{' '}
             </TabButton>
             <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
+              selectTab={() => handleTabChange('certifications')}
+              active={tab === 'certifications'}
             >
-              {" "}
-              Certifications{" "}
+              {' '}
+              Certifications{' '}
             </TabButton>
           </div>
-          <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab).content}
-          </div>
+          <div className='mt-8'>{TAB_DATA.find(t => t.id === tab).content}</div>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default AboutSection;
+export default AboutSection
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,73 +1,125 @@
-"use client";
-import React from "react";
-import Image from "next/image";
-import { TypeAnimation } from "react-type-animation";
-import { motion } from "framer-motion";
-import Link from "next/link";
+'use client'
+// import { useState } from 'react'
+import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation'
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+// import { Document, Page, pdfjs } from 'react-pdf'
+import { saveAs } from 'file-saver'
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   'pdfjs-dist/legacy/build/pdf.worker.min.js',
+//   import.meta.url
+// ).toString()
+
+// import 'react-pdf/dist/Page/AnnotationLayer.css'
+// import 'react-pdf/dist/Page/TextLayer.css'
 
 const HeroSection = () => {
+  const pdfPath = '/yves.pdf'
+  // const [numPages, setNumPages] = useState(null)
+  // const [pageNumber, setPageNumber] = useState(1)
+  // const [showPDF, setShowPDF] = useState(false)
+
+  // function onDocumentLoadSuccess ({ numPages }) {
+  //   setNumPages(numPages)
+  // }
+
+  // function handleChangePage (offset) {
+  //   setPageNumber(prevPageNumber => prevPageNumber + offset)
+  // }
+
+  // function handleButtonClick () {
+  //   setShowPDF(true)
+  // }
+
+  function handleDownload () {
+    saveAs(pdfPath, 'JYMarin-CV.pdf')
+  }
   return (
-    <section className="lg:py-16">
-      <div className="grid grid-cols-1 sm:grid-cols-12">
+    <section className='lg:py-16'>
+      <div className='grid grid-cols-1 sm:grid-cols-12'>
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+          className='col-span-8 place-self-center text-center sm:text-left justify-self-start'
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "}
+          <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold'>
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-primary-500 to-secondary-200'>
+              Hello, I&apos;m{' '}
             </span>
             <br></br>
             <TypeAnimation
-              sequence={[
-                "Judy",
-                1000,
-                "Web Developer",
-                1000,
-                "Mobile Developer",
-                1000,
-                "UI/UX Designer",
-                1000,
-              ]}
-              wrapper="span"
+              sequence={['Yves', 1000, 'Web Developer', 1000]}
+              wrapper='span'
               speed={50}
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+          <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
+            Empowering your web identity. I love developing web applications
+            that help businesses.
           </p>
           <div>
             <Link
-              href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+              href='/#contact'
+              className='px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white'
             >
               Hire Me
             </Link>
             <Link
-              href="/"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              href='/'
+              className='px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3'
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              <span
+                onClick={handleDownload}
+                className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'
+              >
                 Download CV
               </span>
             </Link>
+            {/* <div>
+              <button onClick={handleButtonClick}>Display PDF</button>
+              {showPDF && (
+                <div>
+                  <Document
+                    file={pdfPath}
+                    onLoadSuccess={onDocumentLoadSuccess}
+                  >
+                    <Page pageNumber={pageNumber} />
+                  </Document>
+                  <p>
+                    Page {pageNumber} of {numPages}
+                  </p>
+                  <button
+                    onClick={() => handleChangePage(-1)}
+                    disabled={pageNumber <= 1}
+                  >
+                    Previous
+                  </button>
+                  <button
+                    onClick={() => handleChangePage(1)}
+                    disabled={pageNumber >= numPages}
+                  >
+                    Next
+                  </button>
+                </div>
+              )}
+            </div> */}
           </div>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-4 place-self-center mt-4 lg:mt-0"
+          className='col-span-4 place-self-center mt-4 lg:mt-0'
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
             <Image
-              src="/images/hero-image.png"
-              alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              priority={true}
+              src='/webdev-min.png'
+              alt='hero image'
+              className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-auto h-auto'
               width={300}
               height={300}
             />
@@ -75,7 +127,7 @@ const HeroSection = () => {
         </motion.div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default HeroSection;
+export default HeroSection
