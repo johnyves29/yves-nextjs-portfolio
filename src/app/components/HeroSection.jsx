f6a01419aa5c39afeebb5aff5aeb39b5273a39e4
yvesmarin <johnyvesmarin@gmail.com>
2024-05-02 04:29:52 +0800

    update images

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -117,7 +117,7 @@ const HeroSection = () => {
           <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
             <Image
               priority={true}
-              src='/webdev-min.png'
+              src='/webdev-min.webp'
               alt='hero image'
               className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-auto h-auto'
               width={300}
